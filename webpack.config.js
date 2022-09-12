6338390ea8f9290f7fba55c619ffed6e7af4b04e
Iraida22 <iraida.iva22@mail.ru>
2022-09-12 12:26:22 +0300

    feat: add and configure webpack, babel, typescript

---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const buildPath = path.resolve(__dirname, 'dist');
+const srcPath = path.resolve(__dirname, 'src');
+
+const isProd = process.env.NODE_ENV === "production";
+const getSettingsForStyles = (withModules = false) => {
+  return [
+    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+    !withModules
+      ? "css-loader"
+      : {
+        loader: "css-loader",
+        options: {
+          modules: {
+            localIdentName: !isProd
+              ? "[path][name]__[local]"
+              : "[hash:base64]",
+          },
+        },
+      },
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: ["autoprefixer"],
+        },
+      },
+    },
+    'sass-loader'
+  ]
+};
+
+module.exports = {
+  entry: path.join(srcPath, 'index.tsx'),
+  target: isProd ? 'browserslist' : 'web',
+  devtool: isProd ? 'hidden-source-map' : 'eval-source-map',
+  output: {
+    path: buildPath,
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(srcPath, 'index.html')
+    }),
+    !isProd && new ReactRefreshWebpackPlugin(),
+    isProd && new MiniCssExtractPlugin({
+      filename: '[main]-[hash].css'
+    }),
+    new ForkTsCheckerWebpackPlugin,
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.module\.s?css$/,
+        use: getSettingsForStyles(true),
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /\.module\.s?css$/,
+        use: getSettingsForStyles(),
+      },
+      {
+        test: /\.(png | svg | jpg)$/,
+        use: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024
+          }
+        }
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      components: path.join(srcPath, "components"),
+      styles: path.join(srcPath, "styles"),
+      utils: path.join(srcPath, "utils"),
+      store: path.join(srcPath, "store"),
+      projectTypes: path.join(srcPath, "projectTypes"),
+      pages: path.join(srcPath, "App/pages"),
+      App: path.join(srcPath, "App"),
+    },
+  },
+  devServer: {
+    host: '127.0.0.1',
+    port: 9000,
+    hot: true,
+    static: './dist',
+    client: { overlay: false },
+    historyApiFallback: true,
+  }
+}
